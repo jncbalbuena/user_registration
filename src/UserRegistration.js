22a9773f6ed5fc022754e00458e7e9f2dea02e39
jobellebalbuena <jobelleninacbalbuena@gmail.com>
2021-08-25 21:58:10 +0800

    Update UserRegistration.js

--- src/UserRegistration.js
+++ src/UserRegistration.js
@@ -1,5 +1,16 @@
 import React from 'react'
-
+import { UserForm } from './UserForm';
 export const UserRegistration = () => {
-    return <div className='container'>User Registration</div>
-}
+   
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    };
+    return (
+        <div className= 'container'>
+            <div className='mt-3'>
+                <h1><strong>USER REGISTRATION</strong></h1>
+                <UserForm onSubmit={onSubmit}/>
+        </div>
+        </div>
+    );
+};
